122e659ecab59ebcdab0126ecbf25bc7d2950758
yaniv ziperfal <ziperfal123@gmail.com>
2018-12-17 21:06:26 +0200

    second commit

--- consts.js
+++ consts.js
@@ -1,5 +1,16 @@
 module.exports = {
-    MLAB_URL    :   'mongodb://db_usr:db_pass1@ds135704.mlab.com:35704/young_athletes_db',
-    USER_NAME   :   'db_usr' ,
-    USER_PASS   :   'db_pass1'
+    userName    :   'db_usr' ,
+    userPass    :   'db_pass1'
 }
+
+
+
+
+
+
+
+// module.exports = {
+//     MLAB_URL    :   'mongodb://db_usr:db_pass1@ds135704.mlab.com:35704/young_athletes_db',
+//     USER_NAME   :   'db_usr' ,
+//     USER_PASS   :   'db_pass1'
+// }
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 let     express     =   require('express'),
         mongoose    =   require('mongoose'),
         consts      =   require('./consts.js');
-const   PORT        =   3000,
-        URL         =   consts.MLAB_URL
+const   PORT        =   process.env.PORT || 3000,
+        URL         =   `mongodb://${consts.userName}:${consts.userPass}@ds135704.mlab.com:35704/young_athletes_db`;
 
 let YoungAthlete  = require('./youngAthlete');
-        
+
 
 
 let app = express();
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+
 mongoose.connect(URL , {useNewUrlParser: true}).then( () => {
     console.log(`Connected to Mongo!`);
 }, err => {
@@ -17,17 +21,36 @@ mongoose.connect(URL , {useNewUrlParser: true}).then( () => {
 
 
 
-app.get('/' , (req , res) => {
+app.get('/' , (req , res) => {      // ...the function needs to be imported from another modul... 
     console.log(`You are in GET`);
-    YoungAthlete.find( (err , youngAthletes) => {
+    console.log(`${YoungAthlete}`);
+    YoungAthlete.find( {} , (err , youngAthletes) => {
         if (err) 
             console.log(`ERR : ${err}`);
         else {
-            console.log(`The result is : ${youngAthletes}`);
+            console.log(`result : ${youngAthletes}`);
             res.send(JSON.stringify(youngAthletes));
             mongoose.disconnect();
         }
-    })
+    });
 });
+
+app.post('/' , (req , res) => {     // ...the function needs to be imported from another modul... 
+    console.log(`You are in POST`);
+
+    YoungAthlete.update( {id : req.body.id} , { $set:{'sport.best_record' : req.body.bestRecordToUpdate}} , {} , (err , res) => {
+        if (err)
+            console.log(`ERR : ${err}`);
+        else {
+            console.log(`Updated! ${JSON.stringify(res)}`);
+            YoungAthlete.find( {id : req.body.id} , (err , youngAthletes) =>{
+                console.log(`${youngAthletes}`);
+            } )
+        }
+    }) 
+});
+
+
+
 
 app.listen(PORT , () => console.log(`app listening on port ${PORT}!`));
