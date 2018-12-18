9beaae3bf4256421550f03c5d524df9b507de1c7
yaniv ziperfal <ziperfal123@gmail.com>
2018-12-18 12:23:40 +0200

    all together

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-let     express     =   require('express'),
-        mongoose    =   require('mongoose'),
-        consts      =   require('./consts.js');
-const   PORT        =   process.env.PORT || 3000,
-        URL         =   `mongodb://${consts.userName}:${consts.userPass}@ds135704.mlab.com:35704/young_athletes_db`;
+const   express         =   require('express'),
+        mongoose        =   require('mongoose'),
+        url             =   require('url'),
+        consts          =   require('./consts.js'),
+        YoungAthlete    =   require('./youngAthlete.js');
+const   PORT            =   process.env.PORT || 3000,
+        URL             =   `mongodb://${consts.userName}:${consts.userPass}@ds135704.mlab.com:35704/young_athletes_db`;
 
-let YoungAthlete  = require('./youngAthlete');
 
-
-
-let app = express();
+const app = express();
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,7 +20,7 @@ mongoose.connect(URL , {useNewUrlParser: true}).then( () => {
 
 
 
-app.get('/' , (req , res) => {      // ...the function needs to be imported from another modul... 
+app.get('/getAllAthletes' , (req , res) => {      // ...the function needs to be imported from another module... 
     console.log(`You are in GET`);
     console.log(`${YoungAthlete}`);
     YoungAthlete.find( {} , (err , youngAthletes) => {
@@ -35,9 +34,9 @@ app.get('/' , (req , res) => {      // ...the function needs to be imported from
     });
 });
 
-app.post('/' , (req , res) => {     // ...the function needs to be imported from another modul... 
+app.post('/updateRecord' , (req , res) => {     // ...the function needs to be imported from another module... 
     console.log(`You are in POST`);
-
+    // Updating the inner field (='best_record')-> the field is located in the 'sport' object that located in the document..
     YoungAthlete.update( {id : req.body.id} , { $set:{'sport.best_record' : req.body.bestRecordToUpdate}} , {} , (err , res) => {
         if (err)
             console.log(`ERR : ${err}`);
@@ -45,9 +44,24 @@ app.post('/' , (req , res) => {     // ...the function needs to be imported from
             console.log(`Updated! ${JSON.stringify(res)}`);
             YoungAthlete.find( {id : req.body.id} , (err , youngAthletes) =>{
                 console.log(`${youngAthletes}`);
-            } )
+            })
         }
     }) 
+});
+
+
+app.get('/getAthletesBySportNameBestRecord' , (req , res) => {      // ...the function needs to be imported from another module... 
+    console.log(`You are in SUPER_GET`);
+    let urlPart = url.parse(req.url , true);
+    let query   = urlPart.query;
+    YoungAthlete.find( {'sport.sport_name':query.sportName , 'sport.best_record': {$lt:query.bestRecord} } , (err , youngAthletes) => {
+        if (err) 
+            console.log(`ERR : ${err}`);
+        else {
+            res.send(`${JSON.stringify(youngAthletes)}`);
+            console.log(youngAthletes);
+        }
+    });
 });
 
 
